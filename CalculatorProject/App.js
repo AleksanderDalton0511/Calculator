74bc71bc8dc8a7e3d03e4d23f33f140b5e5649fa
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-10-05 14:26:06 +0300

    Scheme works

--- CalculatorProject/App.js
+++ CalculatorProject/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  
+  const [WeightOfPerson, setWeightOfPerson] = useState(75);
+  const [Gender, setGender] = useState(0.7);
+
+  const [DrinkenMl, setDrinkenMl] = useState(50);
+  const [Strongness, setStrongness] = useState(40);
+  const [PureAlcohol, setPureAlcohol] = useState(((DrinkenMl/100)*Strongness));
+  
+  const [AlcoholInBlood, setAlcoholInBlood] = useState(PureAlcohol/(WeightOfPerson*Gender));
+  const [RealAlcoholInBlood, setRealAlcoholInBlood] = useState(AlcoholInBlood-(AlcoholInBlood/100)*8);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{RealAlcoholInBlood.toFixed(1)}</Text>
       <StatusBar style="auto" />
     </View>
   );
