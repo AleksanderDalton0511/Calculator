ff7416cd847d206e49ab04dd78f732a9f3088666
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-10-13 11:30:58 +0300

    checkpoint

--- CalculatorProject/App.js
+++ CalculatorProject/App.js
@@ -1,62 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import React, { useState, useEffect } from 'react';
 import Calculator from "/calculator.js"; 
-import users from "/users.js"; 
-import { NavigationContainer, useNavigation  } from '@react-navigation/native';
+import Users from "/users.js"; 
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [DrinkedAgo, setDrinkedAgo] = useState(0);
-  const MinusPerHour = 0.1;
-  const losen = MinusPerHour*DrinkedAgo;
-  
-  const [WeightOfPerson, setWeightOfPerson] = useState(75);
-  const [Gender, setGender] = useState(0.7);
-
-  const [DrinkenMl, setDrinkenMl] = useState(50);
-  const [Strongness, setStrongness] = useState(40);
-  const PureAlcohol = (DrinkenMl/100)*Strongness;
-  
-  const AlcoholInBlood = PureAlcohol / (WeightOfPerson*Gender);
-  const RealAlcoholInBlood= AlcoholInBlood-(AlcoholInBlood/100)*5;
-  const [LeftAlcohol, setLeftAlcohol] = useState(RealAlcoholInBlood-losen);
-
-  const OutIn = LeftAlcohol/MinusPerHour;
-  const OutInMin = OutIn*60;
-  const PureHours = OutInMin/60;
-  const PureMins = OutInMin% 60;
-
-  if(LeftAlcohol<0){
-    setLeftAlcohol(0);
-  }
-
-  useEffect(() => {
-    (async () => {
-      setLeftAlcohol(RealAlcoholInBlood-losen);
-    })();
-  }, [Strongness, DrinkenMl, DrinkedAgo]);
 
   return (
     <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
-                    name="Calculator"
-                    component={Calculator}
+                    name="Users"
+                    component={Users}
                 />
-                <Stack.Screen name="Users" component={users} />
+                <Stack.Screen name="Calculator" component={Calculator} />
             </Stack.Navigator>
         </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- CalculatorProject/users.js
+++ CalculatorProject/users.js
@@ -1,26 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
 export default function users(){
     const [memory, setMemory] = useState({
-    "username": "test2",
-    "email": "stets",
-    "password": "iy@test"
+    "Name": "",
+    "Gender": "",
+    "Weight": "",
+
   });
   localStorage.setItem("userData", JSON.stringify(memory));
   const data = localStorage.getItem("userData");
   console.log("data: ", JSON.parse(data));
   return(
     <View>
-    <Text>user2:</Text>
+    <Text>Name</Text>
       <TextInput
         style={{backgroundColor: "lightblue"}}
         onChangeText={newText => setMemory(
-        {"username": {newText},
+        {"name": {newText},
         "email": "stets",
         "password": "iy@test"})}
       />
+      <Text>Gender</Text>
+      <TextInput
+        style={{backgroundColor: "lightblue"}}
+        onChangeText={newText => setMemory(
+        {"name": {newText},
+        "email": "stets",
+        "password": "iy@test"})}
+      />
+      <Text>Weight</Text>
+      <TextInput
+        style={{backgroundColor: "lightblue"}}
+        onChangeText={newText => setMemory(
+        {"name": {newText},
+        "email": "stets",
+        "password": "iy@test"})}
+      />
+
+      <TouchableOpacity><Text style={{backgroundColor: "lightgrey"}}>Cancel</Text></TouchableOpacity>
+      <TouchableOpacity><Text style={{backgroundColor: "green"}}>Save</Text></TouchableOpacity>
       </View>
   )
 }
