228add7459aec3d8981b6cf3d95e2a581647765a
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-10-13 11:53:18 +0300

    checkpoint2

--- CalculatorProject/users.js
+++ CalculatorProject/users.js
@@ -3,43 +3,47 @@ import React, { useState, useEffect } from 'react';
 
 export default function users(){
     const [memory, setMemory] = useState({
-    "Name": "",
-    "Gender": "",
-    "Weight": "",
-
+    "Name": {name},
+    "Gender": {gender},
+    "Weight": {weight},
   });
+  
   localStorage.setItem("userData", JSON.stringify(memory));
   const data = localStorage.getItem("userData");
   console.log("data: ", JSON.parse(data));
+
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [weight, setWeight] = useState("");
+
+  function Save(){
+    setMemory({
+      "Name": {name},
+      "Gender": {gender},
+      "Weight": {weight},
+    });
+  }
+
   return(
     <View>
     <Text>Name</Text>
       <TextInput
         style={{backgroundColor: "lightblue"}}
-        onChangeText={newText => setMemory(
-        {"name": {newText},
-        "email": "stets",
-        "password": "iy@test"})}
+        onChangeText={newText => setName(newText)}
       />
       <Text>Gender</Text>
       <TextInput
         style={{backgroundColor: "lightblue"}}
-        onChangeText={newText => setMemory(
-        {"name": {newText},
-        "email": "stets",
-        "password": "iy@test"})}
+        onChangeText={newText => setGender(newText)}
       />
       <Text>Weight</Text>
       <TextInput
         style={{backgroundColor: "lightblue"}}
-        onChangeText={newText => setMemory(
-        {"name": {newText},
-        "email": "stets",
-        "password": "iy@test"})}
+        onChangeText={newText => setWeight(newText)}
       />
 
       <TouchableOpacity><Text style={{backgroundColor: "lightgrey"}}>Cancel</Text></TouchableOpacity>
-      <TouchableOpacity><Text style={{backgroundColor: "green"}}>Save</Text></TouchableOpacity>
+      <TouchableOpacity onPress={Save}><Text style={{backgroundColor: "green"}}>Save</Text></TouchableOpacity>
       </View>
   )
 }
