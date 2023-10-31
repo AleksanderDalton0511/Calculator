6f4569fd07ea7297b1a1f328c8a6dfcb548f4695
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-10-31 13:51:33 +0200

    fdsaf

--- CalculatorProject/calculator.js
+++ CalculatorProject/calculator.js
@@ -149,10 +149,10 @@ export default function Calculator() {
         <DataTable.Cell><TouchableOpacity><Image style={{width: 30, height: 30, marginLeft: "91%", marginTop: "25%"}} source={require("./assets/settings_icon.png")}></Image></TouchableOpacity></DataTable.Cell> 
       </DataTable.Row>
       
-      <DataTable.Row style={{borderBottomWidth: 0, marginTop: "10%", marginRight: "10%"}}> 
+      <DataTable.Row style={{borderBottomWidth: 0, marginTop: "10%", marginRight: "24%"}}> 
         <DataTable.Cell></DataTable.Cell> 
-        <DataTable.Cell><Text style={{fontSize: 16, color: "white", borderWidth: 1, borderColor: "white", width: "120%", backgroundColor: "white", borderTopLeftRadius: 8, borderBottomLeftRadius: 8, color: "green"}}>   REAL TIME</Text></DataTable.Cell> 
-        <DataTable.Cell><Text style={{fontSize: 16, color: "white", borderWidth: 1, borderColor: "white", borderTopRightRadius: 8, borderBottomRightRadius: 8, width: "140%"}}>           PLAN</Text></DataTable.Cell> 
+        <DataTable.Cell><TouchableOpacity><Text style={{fontSize: 16, color: "white", borderWidth: 1, borderColor: "white", width: "120%", backgroundColor: "white", borderTopLeftRadius: 8, borderBottomLeftRadius: 8, color: "green"}}>    REAL TIME</Text></TouchableOpacity></DataTable.Cell> 
+        <DataTable.Cell><TouchableOpacity><Text style={{fontSize: 16, color: "white", borderWidth: 1, borderColor: "white", borderTopRightRadius: 8, borderBottomRightRadius: 8, width: "140%"}}>                   PLAN</Text></TouchableOpacity></DataTable.Cell> 
         <DataTable.Cell></DataTable.Cell> 
       </DataTable.Row>
 
