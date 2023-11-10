ed3eda7129272b25119a8e9369291987ce34eb11
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-11-10 16:13:40 +0200

    decreasing works

--- CalculatorProject/calculator.js
+++ CalculatorProject/calculator.js
@@ -275,28 +275,6 @@ export default function Calculator() {
     
   }, [number]);
 
-    setTimeout(() => {
-        setFinalHolder(finalHolder-0.00005);
-
-        const storage = new Storage({
-          size: 1000,
-          storageBackend: AsyncStorage,
-          defaultExpires: null,
-          enableCache: true,
-          sync: {
-          }
-
-        });
-        storage.save({
-          key: 'FinalResultBAC', // Note: Do not use underscore("_") in key!
-          data: {
-            TheResult: {finalHolder},
-          },
-          expires: null
-        });
-
-    }, 1000);
-
     if (finalHolder == 0 || finalHolder < 0){
       drinkHelper = 0;
     }
